8129460ae760a8631573b86105118345348211e4
George-Laptop <glthomas526@gmail.com>
2024-01-09 11:47:35 -0500

    added light and dark mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,33 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { ConfigProvider, theme } from "antd";
+import type { AppProps } from "next/app";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [currentTheme, setTheme] = useState("dark");
+
+  return (
+    <div
+      className={
+        currentTheme == "dark"
+          ? "bg-slate-900 w-full h-full transition-all"
+          : "transition-all"
+      }
+    >
+      <ConfigProvider
+        theme={{
+          algorithm:
+            currentTheme == "dark"
+              ? theme.darkAlgorithm
+              : theme.defaultAlgorithm,
+        }}
+      >
+        <Component
+          {...pageProps}
+          currentTheme={currentTheme}
+          setTheme={setTheme}
+        />
+      </ConfigProvider>
+    </div>
+  );
 }
